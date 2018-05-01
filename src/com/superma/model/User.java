3b54f71c112875ead0d7267deaf676453d24702c
tiancaima <442769868@qq.com>
2018-05-01 23:28:47 +0800

    User Modle

---
+++ src/com/superma/model/User.java
@@ -0,0 +1,19 @@
+package com.superma.model;
+
+public class User {
+	private String name;
+	private String power;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPower() {
+		return power;
+	}
+	public void setPower(String power) {
+		this.power = power;
+	}
+}
